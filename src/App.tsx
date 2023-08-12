48e8bb167b702f081b22b9a416ad167c4ce53c66
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-08-12 09:25:20 -0500

    chore: add first version

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { LiteYoutubeEmbed } from 'react-lite-yt-embed'
+
+function App() {
+  return (
+    <>
+      <LiteYoutubeEmbed id='cQIlwKSAyAc' />
+    </>
+  )
+}
+
+export default App
