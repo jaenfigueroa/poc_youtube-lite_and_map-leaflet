5ae1a140ceb6849dd636b3c763f2de49dc6f91ea
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-08-12 09:43:22 -0500

    chore: add params to component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import { LiteYoutubeEmbed } from 'react-lite-yt-embed'
 
 function App() {
   return (
-    <>
-      <LiteYoutubeEmbed id='cQIlwKSAyAc' />
-    </>
+    <div className='container'>
+      <LiteYoutubeEmbed
+        id='cQIlwKSAyAc'
+        // defaultPlay={false}
+        // isPlaylist={ true}
+        // noCookie={true}
+        // mute={true}
+        // isMobile={true}
+        // defaultPlay={true}
+        mobileResolution={'hqdefault'}
+        desktopResolution={'hqdefault'}
+        lazyImage={true}
+        imageAltText='Imagen de prueba'
+        // iframeTitle='Titulo de prueba'
+      />
+    </div>
   )
 }
 
